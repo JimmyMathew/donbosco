e8313134c90518377413ba45fc8d81e0d1264880
jm210792 <jimmy.connect@gmail.com>
2016-02-04 00:32:42 +0530

    Don Bosco
    About page - till Administration

---
+++ head.php
@@ -0,0 +1,83 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Don Bosco polytechnic college</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/responsiveslides.css">
+<script src="js/jquery.min.js"></script>
+<script src="js/responsiveslides.min.js"></script>
+		  <script>
+		    // You can also use "$(window).load(function() {"
+			    $(function () {
+			      // Slideshow 1
+			      $("#slider1").responsiveSlides({
+			        maxwidth: 2500,
+			        speed: 600
+			      });
+			});
+		  </script>
+<!--light-box-->
+<script type="text/javascript" src="js/jquery.lightbox.js"></script>
+<link rel="stylesheet" type="text/css" href="css/lightbox.css" media="screen">
+	<script type="text/javascript">
+		$(function() {
+			$('.gallery a').lightBox();
+		});
+   </script>
+</head>
+<body>
+<div class="header">
+	<div class="wrap">
+		<div class="header-top">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.png" alt=""/></a>
+			</div>
+			<div class="telephone">
+				<span><img src="images/watch.png" "style=margin-right=10px" alt=""/>Timings</span><span class="number">(800) 234-5678</span>
+		    </div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div class="menu">
+		<div class="wrap">
+			<div class="top-nav">
+			      <ul class="nav">
+		            <li class="active"><a href="index.php">Home</a></li>
+					<li><a href="vision&mission.php">About</a></li>
+					<li><a href="gallery.html">Gallery</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<div class="clear"></div>
+				 </ul>
+				  <div class="search">
+				    		<form>
+				    			<input type="text" value="">
+				    			<input type="submit" value="">
+				    		</form>
+					</div>
+					<div class="clear"></div>
+			</div>
+	     </div>
+	</div>
+</div>
+      <!--start-image-slider---->
+		<div class="image-slider">
+		<!-- Slideshow 1 -->
+		  <ul class="rslides" id="slider1">
+		    <li><img src="images/banner1.jpg" alt=""></li>
+			<li><img src="images/banner3.jpg" alt=""></li>
+			<li><img src="images/banner1.jpg" alt=""></li>
+		    <li><img src="images/banner3.jpg" alt=""></li>
+		  </ul>
+	    <!-- Slideshow 2 -->
+	   </div>
+	<!--End-image-slider---->
---
+++ vision&mission.php
@@ -0,0 +1,57 @@
+	
+<?php include 'abouthead.php' ?>
+<div class="cont1 span_2_of_a about_desc">
+				       <h2>Vision</h2>
+		    			   
+				           <p>To innovate and inspire the youth of India.</p> <br/><br/>
+						   <h2>Mission</h2>
+		    			  
+				           <p>We are committed in creating a college of excellence, where we inspire the young to get a technical education at a affordable cost and to provide placement opportunity, which will enhance the innovative thinking within the pupil inorder to make the young India, A Skilled India</p>
+					       <!-- <div class="image group">
+							   <div class="grid images_3_of_1">
+								  <img src="images/pic13.jpg" alt="">
+								</div>
+									<div class="grid span_2_of_1">
+										<h4>Lorem Ipsum is simply dummy text </h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim</p>
+									          <div class="more1">
+								         	  	<a href="#" class="button">Read More</a>
+								         </div>
+							   		</div>
+							   		<div class="clear"></div> 
+			  				 </div>
+			  				 <div class="image group">
+							   <div class="grid images_3_of_1">
+								  <img src="images/pic14.jpg" alt="">
+								</div>
+									<div class="grid span_2_of_1">
+										<h4>Lorem Ipsum is simply dummy text </h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim</p>
+									          <div class="more1">
+								         	  	<a href="#" class="button">Read More</a>
+								         </div>
+							   		</div>
+							   		<div class="clear"></div> 
+			  				 </div>
+			  				 <div class="image group">
+							   <div class="grid images_3_of_1">
+								  <img src="images/pic15.jpg" alt="">
+								</div>
+									<div class="grid span_2_of_1">
+										<h4>Lorem Ipsum is simply dummy text </h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim</p>
+									          <div class="more1">
+								         	  	<a href="#" class="button">Read More</a>
+								         </div>!-->	
+							   		</div>
+							   		<div class="clear"></div> 
+			  				 </div>
+			  		   </div>
+					    <div class="clear"></div> 		
+ </div>
+</div>
+</div>
+					   <?php include 'foot.php' ?>
+					   
+</body>
+</html>
