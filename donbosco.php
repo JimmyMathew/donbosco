816d83f346329f95b7113d14a5d34356e2f95526
Sharon Barron <sweetshalu2803@yahoo.in>
2016-02-04 23:59:32 -0800

    About page - final
    Insert bullets instead of numbers in eduphilosophy. Others are prefect

--- donbosco.php
+++ donbosco.php
@@ -10,11 +10,14 @@
 					  </div>
 										
 											<p>Saint John Bosco, popularly known as Don Bosco(Italian for Father Bosco) was born at Becchi, in Piedmont, Italy on August 16, 1815. From a very young age, he had decided that he had been called to work for the poor boys of that era when Europe was under the grip of the Industrial Revolution, and many young people who came to the cities to study or in search of work fell an easy prey to the many social evils of the time.
-
+<br>
+<br>
 After being ordained a priest of the Catholic Church in 1841, Don Bosco came to the rescue of these poor boys with his novel method of educating youth through total dedication and personal involvement in their lives and problems.
-
+<br>
+<br>
 To ensure that his total dedication to their cause shone through his actions, he based his education on the three great principles of Reason, Religion and Loving-Kindness, loving those to whom he dedicated his life as a caring father, and doing everything possible for their welfare. Don Bosco was attuned to the needs of his society. He did not visualize education in isolation from the community within whose parameters it functioned. Vocational guidance, vocational training, job placement and follow-up were as integral to Don Bosco's scheme of things as they are in modern education.
-
+<br>
+<br>
 The system of education that emerged from these principles of Don Bosco is popularly known as the Preventive System and is the system that is followed in Don Bosco University. Joseph Zoppi, the Swiss educationist said, "If there ever existed a method of education adapted to inspire confidence and love, it is the method of Don Bosco." The system aims at creating a generation of young men and women steeped not only in sound knowledge-based education but also in a strong value-based education for life.</p><div class="clear"></div><BR/>
 
 
--- eduphilosophy.php
+++ eduphilosophy.php
@@ -7,22 +7,27 @@
 						
 										
 											<p>Every educational system is based on some philosophy. We who have inherited the Salesian educational system may therefore ask: what is the philosophical basis of our system? It is a well known fact that Don Bosco was not a theoretician. He never elaborated a complete and organic educational treatise, nor did he ever express his educative method in a completely systematic way. Still, Don Bosco did have theoretical interests, and was interested in pedagogical problematics, even though not in an academic way.Clearly, he did have a method, and it is recognized that his educative efforts did form a system.
-
+<br>
+<br>
 “Preventive System as the Preferential option for the poor” has impelled us in all our educative interventions at the school and college levels. The same choice now calls us to provide diploma courses to the youth in Basin Bridge,a large slum whose inhabitants are settlers from rural Tamil Nadu and from the neighboring states and who had come in search of employment. , disadvantaged because of poor economic and social conditions.
-
+<br>
+<br>
 We are called to accompany the young in the crucial moments of their life. Technical study is a time in their life in which they make the more important decisions for their future. Vocational orientation in the fundamental choices of their life or profession is required most at this stage.
 
 There is a need for a presence of the Salesian Congregation in those environments in which social change, especially in relation to youth is being promoted. We spend so much of our time, energy and means in reacting, counteracting and correcting negative youth culture. Preventive System calls us to be present in the very places where such culture is created.
-
+<br>
+<br>
 Don Bosco Polytechnic College together with Industrial Training Centre can contribute to the qualified formation of youth for entry into the world of work and for their responsible involvement in society in such a way that their involvement goes beyond the demands and needs of the market and produce changes and new developments in society itself.
-
+<br>
+<br>
 Don Bosco Polytechnic College is engaged in a Technical reflection on the Salesian educative system, both in theory and in practice, and its interface is with the world of technology and culture, as an attempt to make a specifically Salesian contribution in the field of education.
+<br>
 
-A preferential option for young people, especially those coming from the lower strata of society.
-A close attention to the relationship between culture, science, technology, education, professionalism and integrity of life and religion (reason and religion). Close attention to building a moral conscience based on values, with special emphasis on a culture of solidarity, through a model of sustainable human development, of equality and reciprocity in relationships, and of human life in its highest dignity.
-Teaching, training, study and cultural services aimed at knowing better the situation of youth, especially the most disadvantaged and bring about their positive transformation.
-A spirit of family characterized by the presence of teachers and administrators among and for the students. Presence of a generous willingness to serve and an atmosphere rich in human values.
-An academic and educative style of relationship founded on love, manifested to and perceived as such by the students (loving kindness). Efforts to promote in society formative programmes that impact on educative processes as well as on strategies and policies for youth. Special attention to the field of education, the formation of educators, the field of technology and work, and the world of communication.</p><div class="clear"></div><BR/>
+ 1. A preferential option for young people, especially those coming from the lower strata of society. <br> 
+ 2. A close attention to the relationship between culture, science, technology, education, professionalism and integrity of life and religion (reason and religion). Close attention to building a moral conscience based on values, with special emphasis on a culture of solidarity, through a model of sustainable human development, of equality and reciprocity in relationships, and of human life in its highest dignity. <br> 
+ 3. Teaching, training, study and cultural services aimed at knowing better the situation of youth, especially the most disadvantaged and bring about their positive transformation.<br>
+ 4. A spirit of family characterized by the presence of teachers and administrators among and for the students. Presence of a generous willingness to serve and an atmosphere rich in human values. <br>
+ 5. An academic and educative style of relationship founded on love, manifested to and perceived as such by the students (loving kindness). Efforts to promote in society formative programmes that impact on educative processes as well as on strategies and policies for youth. Special attention to the field of education, the formation of educators, the field of technology and work, and the world of communication. <br> </p><div class="clear"></div><BR/>
 
 
 
